d1441043575f880d44b826ec4cbbc38c4e11555c
Sungjoon Park <sungjoon512@gmail.com>
2021-10-02 07:26:59 +0900

    refactor main script

--- js/script.js
+++ js/script.js
@@ -1,18 +1,62 @@
-'use strict';
+'use strict'
 
-/* PAGETOP */
-$(function(){
-  var pagetop = $('#page-top');
-  pagetop.hide();
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      pagetop.fadeIn();
-    } else {
-      pagetop.fadeOut();
+document.addEventListener('DOMContentLoaded', () => {
+  handleWordOfToday()
+  handleInternalLink()
+  handleScrollToTop()
+})
+
+function handleWordOfToday() {
+  const wordOfToday = document.getElementById('word-of-today')
+  const image = document.createElement('img')
+
+  image.src = `img/day/${new Date().getDate()}.jpg`
+  image.alt = '今日のコトバ'
+  image.width = 443
+
+  wordOfToday.appendChild(image)
+}
+
+function handleInternalLink() {
+  const menu = document.getElementById('menu')
+  const menuItems = menu.querySelectorAll('a')
+
+  menuItems.forEach(item => {
+    const link = item.getAttribute('href')
+
+    if (!link.startsWith('#') || link === '#') {
+      return
     }
-  });
-  pagetop.click(function () {
-    $('body, html').anitime({ scrollTop: 0}, 500);
-    return false;
-  });
-});
+
+    const targetId = link.split('#')[1]
+    const targetElement = document.getElementById(targetId)
+
+    item.addEventListener('click', (ev) => {
+      ev.preventDefault()
+
+      const rectTop = targetElement.getBoundingClientRect().top
+      const offsetTop = window.pageYOffset
+      const buffer = 0
+      const top = rectTop + offsetTop - buffer
+
+      window.scrollTo({
+        top,
+        behavior: 'smooth'
+      })
+    })
+  })
+}
+
+function handleScrollToTop() {
+  const pageTopButton = document.getElementById('page-top')
+
+  pageTopButton.addEventListener('click', (ev) => {
+    ev.preventDefault()
+
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    })
+  })
+}
